8b145bbc5af8395ce5c89aef71960e8544e82b63
EduardoSig2210 <0001252@uca.edu.sv>
2023-05-23 17:11:17 -0600

    Clase 23 del 5

--- clase23-5/clase23-5/clase23-5.cpp
+++ clase23-5/clase23-5/clase23-5.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-    int arreglo[4] = {1,2,3,4};
+    int arreglo[4] = {1,3,5,4};
 
-    arreglo[2];
+    cout << arreglo[3];
+    cout << arreglo[2];
+    arreglo[2] = 8;
+    cout << arreglo[2]<<endl;
+
+    for (int i = 3; i >= 0; i--)
+    {
+        cout << arreglo[i];
+    }
 }
